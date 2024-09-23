1a842a1812a7d842c37c6b54511126f1a75c9dd1
Yusuf <yusufihsan059@gmail.com>
2024-09-23 12:19:21 +0300

    Detsa-Theme Created

---
+++ tests/Feature/DetsaThemeTest.php
@@ -0,0 +1,10 @@
+<?php namespace Visiosoft\DetsaTheme\Test\Feature;
+
+class DetsaThemeTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
